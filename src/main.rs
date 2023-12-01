e8c086a56b930e1fe12b5d809456fae03b201046
Shashi Srinath <atshashisrinath@gmail.com>
2023-12-01 22:29:00 +0530

    finish day_1_trebuchet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::ops::Add;
+use crate::util::fs_util::read_lines;
+
+mod util;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", day_1_trebuchet());
+}
+
+fn day_1_trebuchet() -> i32 {
+    let lines = read_lines("./inputs/day_1/values.txt").unwrap();
+
+    let mut  calibration_sum = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        let digits_vec: Vec<&str> = line.matches(char::is_numeric).collect();
+        let calibration_value: i32 = format!("{}{}", digits_vec.first().unwrap(), digits_vec.last().unwrap()).parse().unwrap();
+        calibration_sum =  calibration_sum + calibration_value;
+    }
+
+    return calibration_sum;
 }
