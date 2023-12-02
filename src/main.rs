95868b32c0515bcabfcb6173795fe80cc984a192
Shashi Srinath <atshashisrinath@gmail.com>
2023-12-02 11:25:13 +0530

    finished day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::util::fs_util::read_lines;
 mod util;
 
 fn main() {
-    println!("{}", day_1_trebuchet_part2());
+    println!("{}", day_2_cube_conundrum());
 }
 
 fn day_1_trebuchet() -> i32 {
@@ -68,4 +68,57 @@ fn day_1_trebuchet_part2() -> i32 {
     }
 
     return calibration_sum;
+}
+
+fn day_2_cube_conundrum() -> i32 {
+    let lines = read_lines("./inputs/day_2/values.txt").unwrap();
+
+    const MAX_RED_CUBES: i32 = 12;
+    const MAX_GREEN_CUBES: i32 = 13;
+    const MAX_BLUE_CUBES: i32 = 14;
+
+    let mut sum_of_ids = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+
+        let (game, inputs) = line.split_once(":").unwrap();
+
+        let (_, game_id) = game.trim().split_once(" ").unwrap();
+        let attempts: Vec<&str> = inputs.split(";").collect();
+
+        let mut game_is_possible = true;
+
+        for attempt in attempts {
+            let inputs: Vec<&str> = attempt.split(",").collect();
+
+            for input in inputs {
+                let (count, color) = input.trim().split_once(" ").unwrap();
+
+                let count = count.parse::<i32>().unwrap();
+
+                let is_valid = match color {
+                    "red" => count <= MAX_RED_CUBES,
+                    "green" => count <= MAX_GREEN_CUBES,
+                    "blue" => count <= MAX_BLUE_CUBES,
+                    _ => false,
+                };
+
+                if !is_valid {
+                    game_is_possible = false;
+                    break;
+                }
+            }
+
+            if !game_is_possible {
+                break;
+            }
+        }
+
+        if game_is_possible {
+            sum_of_ids = sum_of_ids + game_id.parse::<i32>().unwrap();
+        }
+    }
+
+    sum_of_ids
 }
