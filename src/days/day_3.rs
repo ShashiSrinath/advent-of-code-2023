ce2ffa65b2836745eaa24014cef78f3026f452be
Shashi Srinath <atshashisrinath@gmail.com>
2023-12-03 13:35:47 +0530

    clean duplicate code

--- src/days/day_3.rs
+++ src/days/day_3.rs
@@ -116,58 +116,6 @@ pub fn day_3_gear_ratios() -> i32 {
         matrix.push(current_vec);
     }
 
-    impl PartInput {
-        pub fn is_a_part(&self, matrix: &Vec<Vec<char>>) -> bool {
-            let max_y = matrix.len() - 1;
-            let max_x = matrix.get(0).expect("Invalid input matrix").len() - 1;
-
-            let mut coordinate_matrix: Vec<(i32, i32)> = Vec::new();
-
-            let top_row = self.coords.y - 1;
-            let bottom_row = self.coords.y + 1;
-
-            if top_row >= 0 {
-                for x in (self.coords.x - 1)..(self.coords.x2 + 2) {
-                    if x >= 0 && x <= max_x as i32 {
-                        coordinate_matrix.push((x, top_row));
-                    }
-                }
-            }
-
-            if self.coords.y >= 0 {
-                if self.coords.x - 1 >= 0 {
-                    coordinate_matrix.push((self.coords.x - 1, self.coords.y))
-                }
-
-                if self.coords.x2 + 1 <= max_x as i32 {
-                    coordinate_matrix.push((self.coords.x2 + 1, self.coords.y))
-                }
-            }
-
-            if bottom_row <= max_y as i32 {
-                for x in (self.coords.x - 1)..(self.coords.x2 + 2) {
-                    if x >= 0 && x <= max_x as i32 {
-                        coordinate_matrix.push((x, bottom_row));
-                    }
-                }
-            }
-
-            for (coord_x, coord_y) in coordinate_matrix {
-                let element = matrix
-                    .get(coord_y as usize)
-                    .unwrap()
-                    .get(coord_x as usize)
-                    .unwrap();
-
-                if !element.is_numeric() && !element.eq(&'.') {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    }
-
     let mut parts: Vec<PartInput> = Vec::new();
 
     for (y, vec_x) in matrix.iter().enumerate() {
